3ee6ba0e2628272d3cd02f336a17d98a7b0e33b1
hasan <hasanbatis440@gmail.com>
2021-05-11 02:04:07 +0300

    implement stock tracking restrictions and add functionality to remove tracking stocks

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -10,4 +10,11 @@ class UserStocksController < ApplicationController
     redirect_to my_protfolio_path
 
   end
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed"
+    redirect_to my_protfolio_path
+  end
 end
