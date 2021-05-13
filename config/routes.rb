a41a414c0afb71a4bb4ec6442e07d34c2f0dffca
hasan <hasanbatis440@gmail.com>
2021-05-13 03:06:38 +0300

    finalizing finance tracker app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
   resources :user_stocks, only: [:create, :destroy]
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my_protfolio', to: 'users#my_protfolio'
   get 'search_stock', to: 'stocks#search'
+
+  get 'search_friend', to: 'users#search'
+  get 'friends', to: 'users#friends'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 
 
 end
